656c06c340317a8ad35dca21a7a9ae4f2868468d
hamzaelalamy <hamza.elalamy1@uit.ac.ma>
2025-05-06 23:49:01 +0100

    Enhance candidate service configuration: add Spring Boot version management, include Jersey dependencies, and introduce Eureka client configuration

---
+++ candidate-service/src/main/java/com/recruitment/candidateservice/config/EurekaClientConfig.java
@@ -0,0 +1,22 @@
+package com.recruitment.candidateservice.config;
+
+import org.springframework.cloud.netflix.eureka.http.WebClientTransportClientFactories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.function.Supplier;
+
+@Configuration
+public class EurekaClientConfig {
+
+    @Bean
+    public WebClientTransportClientFactories transportClientFactories() {
+        return new WebClientTransportClientFactories(webClientBuilderSupplier());
+    }
+
+    @Bean
+    public Supplier<WebClient.Builder> webClientBuilderSupplier() {
+        return WebClient::builder;
+    }
+}
